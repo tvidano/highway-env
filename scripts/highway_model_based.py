4491a1a8704a8f0e851f724836e91cd7aaab1ce7
tvidano <61514829+tvidano@users.noreply.github.com>
2021-07-11 22:48:29 -0700

    Comment out plots and move config to environment. Still debugging.

--- scripts/highway_model_based.py
+++ scripts/highway_model_based.py
@@ -38,42 +38,28 @@ if 'highway_env' not in sys.modules:
 # ==================================
 
 if __name__ == "__main__":
-    #env = highway_env.envs.merge_env.MergeEnv()
     env = highway_env.envs.collision_env.CollisionEnv()
-    #env = highway_env.envs.highway_env.HighwayEnv()
-    #env = highway_env.envs.exit_env.ExitEnv()
-    #env = highway_env.envs.two_way_env.TwoWayEnv()
-    #env = highway_env.envs.lane_keeping_env.LaneKeepingEnv() # can't get this to work with manual
-    #env = highway_env.envs.u_turn_env.UTurnEnv()
-    #env = gym.make('highway-v0')
 
-    env.configure({
-        "manual_control": False,
-        # "action": {
-        #     "type": "ContinuousAction", # DiscreteMetaAction
-        #     "dynamical": False
-        # },
-        "simulation_frequency": 15,  # [Hz]
-        "policy_frequency": 10,  # [Hz]
-        "offroad_terminal": True,
-        "lanes_count": 3,
-        "vehicles_density": 2,
-        "vehicles_count": 20,
-        "controlled_vehicles": 1,
-        "screen_width": 900,  # [px]
-        "screen_height": 150,  # [px]
-        "other_vehicles_type": "highway_env.vehicle.behavior.IDMVehicle",
-        "stopping_vehicles_count": 2,
-        "duration": 10,  # [s]
-    })
+    # Override standard configuration
+    # env.configure({
+    #     "manual_control": False,
+    #     "simulation_frequency": 15,  # [Hz]
+    #     "policy_frequency": 10,  # [Hz]
+    #     "vehicles_density": 2,
+    #     "vehicles_count": 20,
+    #     "stopping_vehicles_count": 2,
+    #     "duration": 10,  # [s]
+    # })
 
-    #check_env(env)
+    # Uncomment to check environment with OpenAi Gym:
+    # check_env(env)
 
-    #model = PPO("MlpPolicy", env, learning_rate=0.0003, n_steps=2048,
+    # Uncomment to try training an PPO algorithm on this environemt:
+    # model = PPO("MlpPolicy", env, learning_rate=0.0003, n_steps=2048,
     #            batch_size=64, n_epochs=10,verbose=1)
-    #model.learn(total_timesteps=25000, )
-    #model.save("ppo_collision")
-    #model.load("ppo_collision")
+    # model.learn(total_timesteps=25000, )
+    # model.save("ppo_collision")
+    # model.load("ppo_collision")
 
     obs = env.reset()
     env.render()
@@ -84,44 +70,45 @@ if __name__ == "__main__":
     slips = []
     ttc = []
     done = False
+    #TODO{tvidano}: Figure out why vehicles no longer stop
     while not done:
-        #action = env.action_space.sample()
-        action = np.array([-0.5, 0.0])
+        # Use just hard braking (will probably lock the wheel):
+        action = np.array([-1, 0.0])
+
+        # Uncomment to use RL algorithm actions:
         #action, _states = model.predict(obs)
+
         obs, rew, done, info = env.step(action)
-        velocity.append(info["speed"])
-        forces.append(info["tire_forces"])
-        slips.append(info["slip_values"])
-        ttc.append(info["ttc"])
+        # velocity.append(info["speed"])
+        # forces.append(info["tire_forces"])
+        # slips.append(info["slip_values"])
+        # ttc.append(info["ttc"])
         rewards.append(rew)
-        #if done:
-        #    print(f'Finished after {t+1} steps.')
-        #    break
         
     env.close()
-    velocity = np.vstack(velocity)
-    forces = np.vstack(forces)
-    slips = np.vstack(slips)
+    # velocity = np.vstack(velocity)
+    # forces = np.vstack(forces)
+    # slips = np.vstack(slips)
     
-    plt.figure()
-    plt.subplot(231)
-    plt.plot(velocity[:,0])
-    plt.title('Long. Vel.')
-    plt.subplot(232)
-    plt.plot(velocity[:,2])
-    plt.title('Front Omega')
-    plt.subplot(233)
-    plt.plot(forces[:,0])
-    plt.title('Front Fx')
-    plt.subplot(234)
-    plt.plot(forces[:,1])
-    plt.title('Front Fy')
-    plt.subplot(235)
-    plt.plot(slips[:,0])
-    plt.title('Front kappa')
-    plt.subplot(236)
-    plt.plot(slips[:,1])
-    plt.title('Front Alpha')
-    plt.figure()
-    plt.plot(ttc)
-    plt.show()
+    # plt.figure()
+    # plt.subplot(231)
+    # plt.plot(velocity[:,0])
+    # plt.title('Long. Vel.')
+    # plt.subplot(232)
+    # plt.plot(velocity[:,2])
+    # plt.title('Front Omega')
+    # plt.subplot(233)
+    # plt.plot(forces[:,0])
+    # plt.title('Front Fx')
+    # plt.subplot(234)
+    # plt.plot(forces[:,1])
+    # plt.title('Front Fy')
+    # plt.subplot(235)
+    # plt.plot(slips[:,0])
+    # plt.title('Front kappa')
+    # plt.subplot(236)
+    # plt.plot(slips[:,1])
+    # plt.title('Front Alpha')
+    # plt.figure()
+    # plt.plot(ttc)
+    # plt.show()
