49d5145a80cbf0dfe5566e2e6fb73645dc916722
tvidano <61514829+tvidano@users.noreply.github.com>
2021-07-12 19:32:25 -0700

    Clean up highway_model_based.py.

--- scripts/highway_model_based.py
+++ scripts/highway_model_based.py
@@ -40,15 +40,14 @@ if 'highway_env' not in sys.modules:
 if __name__ == "__main__":
     env = highway_env.envs.collision_env.CollisionEnv()
 
-    # Override standard configuration
+    # Recommended Environment Hypertuning Parameters:
     # env.configure({
-    #     "manual_control": False,
-    #     "simulation_frequency": 15,  # [Hz]
-    #     "policy_frequency": 10,  # [Hz]
+    #     "duration": 10,  # [s]
+    #     "stopping_vehicles_count": 5,
+    #     "time_to_intervene": 2, # [s]
+    #     "time_after_collision": 0, # [s]
     #     "vehicles_density": 2,
     #     "vehicles_count": 20,
-    #     "stopping_vehicles_count": 2,
-    #     "duration": 10,  # [s]
     # })
 
     # Uncomment to check environment with OpenAi Gym:
@@ -70,7 +69,6 @@ if __name__ == "__main__":
     slips = []
     ttc = []
     done = False
-    #TODO{tvidano}: Figure out why vehicles no longer stop
     while not done:
         # Use just hard braking (will probably lock the wheel):
         action = np.array([-1, 0.0])
@@ -79,36 +77,36 @@ if __name__ == "__main__":
         #action, _states = model.predict(obs)
 
         obs, rew, done, info = env.step(action)
-        # velocity.append(info["speed"])
-        # forces.append(info["tire_forces"])
-        # slips.append(info["slip_values"])
-        # ttc.append(info["ttc"])
+        velocity.append(info["speed"])
+        forces.append(info["tire_forces"])
+        slips.append(info["slip_values"])
+        ttc.append(info["ttc"])
         rewards.append(rew)
         
     env.close()
-    # velocity = np.vstack(velocity)
-    # forces = np.vstack(forces)
-    # slips = np.vstack(slips)
+    velocity = np.vstack(velocity)
+    forces = np.vstack(forces)
+    slips = np.vstack(slips)
     
-    # plt.figure()
-    # plt.subplot(231)
-    # plt.plot(velocity[:,0])
-    # plt.title('Long. Vel.')
-    # plt.subplot(232)
-    # plt.plot(velocity[:,2])
-    # plt.title('Front Omega')
-    # plt.subplot(233)
-    # plt.plot(forces[:,0])
-    # plt.title('Front Fx')
-    # plt.subplot(234)
-    # plt.plot(forces[:,1])
-    # plt.title('Front Fy')
-    # plt.subplot(235)
-    # plt.plot(slips[:,0])
-    # plt.title('Front kappa')
-    # plt.subplot(236)
-    # plt.plot(slips[:,1])
-    # plt.title('Front Alpha')
-    # plt.figure()
-    # plt.plot(ttc)
-    # plt.show()
+    plt.figure()
+    plt.subplot(231)
+    plt.plot(velocity[:,0])
+    plt.title('Long. Vel.')
+    plt.subplot(232)
+    plt.plot(velocity[:,2])
+    plt.title('Front Omega')
+    plt.subplot(233)
+    plt.plot(forces[:,0])
+    plt.title('Front Fx')
+    plt.subplot(234)
+    plt.plot(forces[:,1])
+    plt.title('Front Fy')
+    plt.subplot(235)
+    plt.plot(slips[:,0])
+    plt.title('Front kappa')
+    plt.subplot(236)
+    plt.plot(slips[:,1])
+    plt.title('Front Alpha')
+    plt.figure()
+    plt.plot(ttc)
+    plt.show()
