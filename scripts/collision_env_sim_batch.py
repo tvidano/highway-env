8f727a500d4caa361e37f2b5acfdeb5ef449fed7
Nihal <nihalsgulati@gmail.com>
2021-07-20 11:57:35 -0700

    small changes, add models trained

--- scripts/collision_env_sim_batch.py
+++ scripts/collision_env_sim_batch.py
@@ -69,12 +69,13 @@ if __name__ == "__main__":
     totalruns = 100  # number of runs, obviously
     render_env = True  # whether to render the car
     report_every = 10  # how often to report running progress Ex. every 5th run
-    do_training = True # whether to train a new model or use a saved one
-    model_name = 'TQC' # choose from:  'baseline' = deterministic hard braking, no steering always
+    do_training = False # whether to train a new model or use a saved one
+    model_name = 'PPO' # choose from:  'baseline' = deterministic hard braking, no steering always
                                         #   'ppo' = implements trained PPO if available, otherwise trains a PPO
                                         #   'a2c' = implements trained A2C if available, otherwise trains an A2C
     model_path = model_name.lower() + "_collision"
     modifier = ''
+    model_path += modifier
 
     reward_stats = []
     num_mitigated = 0
@@ -87,69 +88,69 @@ if __name__ == "__main__":
         model = None
 
     elif model_name == 'PPO':
-        model = PPO("MlpPolicy", env, learning_rate=0.003, n_steps=2048, batch_size=64, n_epochs=20,verbose=1)
+        model = PPO("MlpPolicy", env, learning_rate=0.003, n_steps=2048, batch_size=64, n_epochs=20,verbose=1, device='cuda')
         if do_training:
             start = timeit.default_timer()
-            model.learn(total_timesteps=50000, )
+            model.learn(total_timesteps=500000, )
             model.save(model_name.lower() + "_collision")
             stop = timeit.default_timer()
             print("Training took", stop-start, "seconds.")
-        model.load(model_path+modifier)
+        model.load(model_path)
         print(f'Loaded {model_name} from {os.path.join(os.getcwd(), model_path +".zip")}\n')
 
     elif model_name == 'A2C':
         model = A2C("MlpPolicy", env, learning_rate=0.003, n_steps=2048,verbose=1)
         if do_training:
             start = timeit.default_timer()
-            model.learn(total_timesteps=50000, )
+            model.learn(total_timesteps=500000, )
             model.save(model_name.lower() + "_collision")
             stop = timeit.default_timer()
             print("Training took", stop - start, "seconds.")
-        model.load(model_path+modifier)
+        model.load(model_path)
         print(f'Loaded {model_name} from {os.path.join(os.getcwd(), model_path +".zip")}\n')
 
     elif model_name == 'DDPG':
         model = DDPG("MlpPolicy", env, learning_rate=0.003, batch_size=100, verbose=1)
         if do_training:
             start = timeit.default_timer()
-            model.learn(total_timesteps=50000, )
+            model.learn(total_timesteps=500000, )
             model.save(model_name.lower() + "_collision")
             stop = timeit.default_timer()
             print("Training took", stop - start, "seconds.")
-        model.load(model_path+modifier)
+        model.load(model_path)
         print(f'Loaded {model_name} from {os.path.join(os.getcwd(), model_path + ".zip")}\n')
 
     elif model_name == 'SAC':
         model = SAC("MlpPolicy", env, learning_rate=0.003, batch_size=256, verbose=1)
         if do_training:
             start = timeit.default_timer()
-            model.learn(total_timesteps=50000, )
+            model.learn(total_timesteps=500000, )
             model.save(model_name.lower() + "_collision")
             stop = timeit.default_timer()
             print("Training took", stop - start, "seconds.")
-        model.load(model_path+modifier)
+        model.load(model_path)
         print(f'Loaded {model_name} from {os.path.join(os.getcwd(), model_path + ".zip")}\n')
 
     elif model_name == 'TD3':
         model = TD3("MlpPolicy", env, learning_rate=0.003, batch_size=100, verbose=1)
         if do_training:
             start = timeit.default_timer()
-            model.learn(total_timesteps=50000, )
+            model.learn(total_timesteps=500000, )
             model.save(model_name.lower() + "_collision")
             stop = timeit.default_timer()
             print("Training took", stop - start, "seconds.")
-        model.load(model_path+modifier)
+        model.load(model_path)
         print(f'Loaded {model_name} from {os.path.join(os.getcwd(), model_path + ".zip")}\n')
 
     elif model_name == 'TQC':
         model = TQC("MlpPolicy", env, learning_rate=0.003, batch_size=256, verbose=1)
         if do_training:
             start = timeit.default_timer()
-            model.learn(total_timesteps=50000, )
+            model.learn(total_timesteps=500000, )
             model.save(model_name.lower() + "_collision")
             stop = timeit.default_timer()
             print("Training took", stop - start, "seconds.")
-        model.load(model_path+modifier)
+        model.load(model_path)
         print(f'Loaded {model_name} from {os.path.join(os.getcwd(), model_path + ".zip")}\n')
 
     print("Model", model_name, "trained/loaded")
