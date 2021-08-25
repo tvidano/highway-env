606ff5da9b357151a3d256d29adf0493bc8c92a4
Nihal <nihalsgulati@gmail.com>
2021-08-24 20:42:34 -0700

    simplified step and simulate function
    Now use super() mainly

--- highway_env/envs/collision_env.py
+++ highway_env/envs/collision_env.py
@@ -28,7 +28,7 @@ class CollisionEnv(HighwayEnv):
         config.update({
             "action": {
                 "type": "ContinuousAction",
-                "vehicle_class": "CoupledDynamics",
+                "vehicle_class": CoupledDynamics,
             },
             "observation": {
                 "type": "LidarKinematicObservation",  # "Kinematics" "LidarObservation" "LidarKinematicObservation"
@@ -128,28 +128,7 @@ class CollisionEnv(HighwayEnv):
             chosen_vehicle.target_speed = 0
 
     def step(self, action: Action) -> Tuple[ObservationType, float, bool, dict]:
-        """
-        Perform an action and step the environment dynamics.
-        The action is executed by the ego-vehicle, and all other vehicles on the
-        road performs their default behaviour for several simulation timesteps
-        until the next decision making step.
-        The ego-vehicle is not allowed to act until a collision is imminent.
-        :param action: the action performed by the ego-vehicle
-        :return: a tuple (observation, reward, terminal, info)
-        """
-        if self.road is None or self.vehicle is None:
-            raise NotImplementedError("The road and vehicle must be initialized in the environment implementation")
-
-        self._spin(action)
-        self.steps += 1
-        self._simulate(action)
-
-        obs = self.observation_type.observe()
-        reward = self._reward(action)
-        terminal = self._is_terminal()
-        info = self._info(obs, action)
-
-        return obs, reward, terminal, info
+        return self._spin(action)
 
     def _spin(self, action):
         """Run simulation while vehicle is inactive"""
@@ -169,20 +148,11 @@ class CollisionEnv(HighwayEnv):
         else:
             self.did_run = True
 
+        return super().step(action)
+
     def _simulate(self, action: Optional[Action] = None) -> None:
-        """Perform several steps of simulation with constant action."""
-        for _ in range(int(self.config["simulation_frequency"] // self.config["policy_frequency"])):
-            # Forward action to the vehicle
-            if action is not None and not self.config["manual_control"]:
-                self.action_type.act(action)
-
-            self.road.act()
-            self.road.step(1 / self.config["simulation_frequency"])
-            self.time += (1/self.config["simulation_frequency"])
-
-            # Automatically render intermediate simulation steps if a viewer has been launched
-            # Ignored if the rendering is done offscreen
-            self._automatic_rendering()
+        super()._simulate(action)
+        self.enable_auto_render = True
 
     def _imminent_collision(self) -> bool:
         """Returns True if a collision is about to happen."""
@@ -210,7 +180,7 @@ class CollisionEnv(HighwayEnv):
                 self.active = 2
                 self.time_since_avoidance = self.time
         if self.active == 2:
-            if (self.time - self.time_since_avoidance) > self.config["control_time_after_avoid"]:
+            if (self.time - self.time_since_avoidance) > self.config["control_time_after_avoid"] * self.config["simulation_frequency"]:
                 self.active = 0
         if self.vehicle.crashed:
             self.active = 4
@@ -225,11 +195,11 @@ class CollisionEnv(HighwayEnv):
         :param action: the last action performed
         :return: the corresponding reward
         """
-        duration_reached = self.time >= self.config["duration"]
+        duration_reached = self.time >= self.config["duration"] * self.config["simulation_frequency"]
         reward = self.config["collision_avoided_reward"] if duration_reached and not self.vehicle.crashed and self.vehicle.on_road else 0
-        reward += self.config["time_alive_reward"] * self.time
+        reward += self.config["time_alive_reward"] * self.time / self.config["simulation_frequency"]
         reward += self.config["lateral_velocity_reward"] * abs(self.vehicle.lateral_velocity) if self.active == 2 else 0
-        reward = self.config["offroad_reward"] if not self.on_road else reward
+        reward = self.config["offroad_reward"] if not self.vehicle.on_road else reward
         reward = self.config["crashed_reward"] if self.vehicle.crashed else reward
         return reward
 
@@ -239,12 +209,12 @@ class CollisionEnv(HighwayEnv):
         a collision.
         """
         if self.vehicle.crashed:
-            end_time = self.time + self.config["time_after_collision"]
+            end_time = self.time + self.config["time_after_collision"] * self.config["simulation_frequency"]
             while self.time < end_time:
                 self._simulate(None)
 
         return self.vehicle.crashed or \
-            self.time >= self.config["duration"] or \
+            self.time >= self.config["duration"] * self.config["simulation_frequency"] or \
             (self.config["offroad_terminal"] and not self.vehicle.on_road)
 
     def _info(self, obs: ObservationType, action: Action) -> dict:
@@ -262,7 +232,7 @@ class CollisionEnv(HighwayEnv):
             "speed": np.array([self.vehicle.longitudinal_velocity,
                                self.vehicle.lateral_velocity,
                                self.vehicle.front_wheel_angular_velocity]),
-            "time": self.time,
+            "time": self.time/self.config["simulation_frequency"],
             "tire_forces": self.vehicle.tire_forces,
             "ttc": self.time_to_collision,
             "imminent": self._imminent_collision(),
