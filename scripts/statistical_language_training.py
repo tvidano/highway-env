0471ed02568c8b43adc8b24859018e59dc44ef82
trevor <tvidano@gmail.com>
2023-03-16 23:34:42 -0700

    Implement various ngram models and train them.

--- scripts/statistical_language_training.py
+++ scripts/statistical_language_training.py
@@ -4,17 +4,32 @@ scene will correlate with the number of collisions observed in the scene."""
 
 import numpy as np
 import json
-import sys
 import os.path as op
-from nltk.util import ngrams
-from nltk.util import everygrams
-from nltk.lm.preprocessing import padded_everygram_pipeline, flatten
-from nltk.lm import MLE
+from nltk.util import everygrams, bigrams
+from nltk.lm.preprocessing import flatten
+from nltk.lm import *
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+# Useful resources:
+# https://web.stanford.edu/~jurafsky/slp3/3.pdf
+# https://www.kaggle.com/code/alvations/n-gram-language-model-with-nltk.
+
+
+def unpacked_ngrams(text, n):
+    """Returns ngrams as a list of ngrams (tuples) from |text|."""
+    unpacked_ngrams = []
+    for sentence in text:
+        ngrams = list(everygrams(sentence, max_len=n))
+        for ngram in ngrams:
+            unpacked_ngrams.append(ngram)
+    return unpacked_ngrams
+
 
 # Collect the states from the state record of an experiment.
 experiment = "experiment_009"
 local_highway_env = op.join(op.dirname(op.realpath(__file__)), "..",)
-experiments_dir = op.join(local_highway_env, "scripts", 
+experiments_dir = op.join(local_highway_env, "scripts",
                           "sensor_scheduling_experiments")
 path = op.join(experiments_dir, experiment, "raw_data.json")
 with open(path, "r") as f:
@@ -23,42 +38,68 @@ with open(path, "r") as f:
 
 # Extract scenes from state record.
 scenes = list(state_record.values())
-observed_vocab = []
-for scene in scenes:
-    for state in scene:
-        if state in observed_vocab:
-            continue
-        else:
-            observed_vocab.append(state)
 
-# TODO: Separate out a train and test set. Maybe a validation set?
+# Convert states from integers to strings to support nltk models.
+tokenized_scenes = [[str(state) for state in scene] for scene in scenes]
 
-# Train bigram model with sentence padding.
-training_ngrams, padded_scenes = padded_everygram_pipeline(1, scenes)
-print(len(list(training_ngrams)))
-print(len(list(padded_scenes)))
+# Separate out test and train scenes. Assume no correlation in order of scenes.
+i_split = int(np.floor(len(tokenized_scenes)*0.9))
+train_scenes = tokenized_scenes[:i_split]
+test_scenes = tokenized_scenes[i_split:]
 
-# Train bigram model without sentence padding.
-# TODO: There should be a way to create training_ngrams without sentence
-# padding. Look into padded_everygram_pipeline.
-training_ngrams = everygrams(scenes[0],max_len=2)
-padded_scenes = flatten(scenes)
-print(len(list(training_ngrams)))
-print(len(list(padded_scenes)))
+# # Construct ngrams and flattened scenes iterators.
+# n = 3
+# training_ngrams = [everygrams(list(scene), max_len=n)
+#                    for scene in train_scenes]
+# # Add a word outside the dictionary so that "<UNK>" is seen once. This prevents
+# # discounting models from assigning inf perplexity to data containing unseen
+# # words.
+# training_ngrams.append((tuple("0"), ))
+# flat_train_scenes = flatten(train_scenes)
 
-# TODO: Train maximum likelihood estimator on experiment.
+# # Train maximum likelihood estimator on experiment.
+# # KneserNeyInterpolated fails for some reason. The fix above solves the inf
+# # perplexity problem for the other interpolated models, but not this one. It
+# # takes a very long time to compute perplexity so it is likely that there is
+# # some issue with how I'm trying to use it, or in how it is implemented.
+# model = KneserNeyInterpolated(n)
+# model.fit(training_ngrams, flat_train_scenes)
+# print("\n" + "#"*10 + "Summary" + "#"*10)
+# print(f'Length of vocab: {len(model.vocab)}')
+# print(f'Model.counts = {model.counts}')
+# print(f'The probability of "<UNK>" is: {model.score("<UNK>")}')
 
-# TODO: Test how well the model predicts on training data. A useful resource:
-# https://www.kaggle.com/code/alvations/n-gram-language-model-with-nltk.
+# # Test how well the model predicts on training data.
+# unpacked_train_ngrams = unpacked_ngrams(train_scenes, n)
+# print(f'Training perplexity: {model.perplexity(unpacked_train_ngrams)}')
 
-# TODO: Test how well the model predicts on test data.
+# # Test how well the model predicts on test data.
+# test_ngrams = unpacked_ngrams(test_scenes, n)
+# print(f'Testing perplexity: {model.perplexity(test_ngrams)}')
 
-# TODO: Ultimately we want to fit the best model to the data at this point, 
-# so we need a way to compare different ngrams.
+# Compare different models.
+models = [Laplace, WittenBellInterpolated, AbsoluteDiscountingInterpolated]
+model_names = ["Laplace", "WittenBell", "AbsoluteDiscounting"]
+ns = np.arange(1, 4)
+plt.figure()
+for model, name in zip(models, model_names):
+    for n in ns:
+        flat_train_scenes = flatten(train_scenes)
+        training_ngrams = [everygrams(list(scene), max_len=n)
+                           for scene in train_scenes]
+        training_ngrams.append((tuple("0"), ))
+        m = model(n)
+        m.fit(training_ngrams, flat_train_scenes)
+        test_ngrams = unpacked_ngrams(test_scenes, n)
+        perplexity = m.perplexity(test_ngrams)
+        print(f"{name},{n}: {perplexity}")
+        plt.scatter(n, perplexity, label=name+':'+str(n))
+plt.legend()
+plt.show()
 
-# TODO: Use the best fit model to compute the entropy rate (or the entropy of 
+# TODO: Use the best fit model to compute the entropy rate (or the entropy of
 # each state). Additionally, may want to compute the entropy of the experiment
-# or of the scene. This should be analogous to computing the entropy of each 
+# or of the scene. This should be analogous to computing the entropy of each
 # sentence or the entropy of a corpus.
 
 # TODO: See if the experiment, scene, or state has an entropy that correlates
